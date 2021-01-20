fcde384f40aa9110b01e34e2472cb5b8215e0361
Jared Young <jyoung19db@gmail.com>
2021-01-20 15:27:05 -0600

    Added no space option

--- FileRename/filerename.py
+++ FileRename/filerename.py
@@ -4,35 +4,44 @@ from os.path import isfile, join
 
 
 def main():
-    path = "C:\\Users\\jyoun\\Desktop\\C0D3\\PythonStuff\\FileRename\\TestFiles"
+    #path = "C:\\Users\\jyoun\\Desktop\\C0D3\\PythonStuff\\FileRename\\TestFiles"
     
     print("**********FILE RENAMING TOOL**********")
     path = input("Provide folder path containing files: ")
-    #path = path.replace("\\", "\\\\")
     print(path)
+    onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+    print(f"Files in given directory: {onlyfiles}")
+
     print("Pick a naming convention:")
     print("1. No Spaces (test01)")
     print("2. Underscores (test_01)")
-    print("3. Periods (test.01)")
-    print("4. Uppercase (TEST01)")
-    print("5. Lowercase (test01)")
+    print("3. Uppercase (TEST01)")
+    print("4. Lowercase (test01)")
+    print("5. Rename Individual File")
 
-    user_input = input("Your Choice: ")
-    onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
-    print(onlyfiles)
+    user_input = int(input("Your Choice: "))
 
-    rename(user_input, onlyfiles)
+    rename_file(path, user_input, onlyfiles)
 
-def rename(user_input, onlyfiles):
-    style = {1:"", 2:"_", 3:"."}
+def rename_file(path, user_input, onlyfiles):
 
+    #No Spaces
+    if user_input == 1:
+        for i in range(len(onlyfiles)):
+            if " " in onlyfiles[i]:
+                #onlyfiles[i] = onlyfiles[i].replace(" ", "")
+                os.rename(path + "\\" + onlyfiles[i], path + "\\" + onlyfiles[i].replace(" ", ""))
+
+    #UnderScores
+    elif user_input == 2:
+        pass
     #UpperCase
-    if user_input == 4:
+    elif user_input == 3:
         pass
     #LowerCase
-    elif user_input == 5:
+    elif user_input == 4:
         pass
-    #1-3
+    #Rename Individual
     else:
         pass
 
